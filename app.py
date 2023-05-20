3e3e36d91718eecc0a2733b5a45c96d673454e73
Jan N <jan.novopacky@gmail.com>
2023-05-20 10:31:54 +0200

    split process_data

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-webhook_url = os.environ.get('WEBHOOK_URL') 
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
+
+def process_common_data(company, company_ares):
+    change_company_data(
+        company_id=company.id_pipedrive,
+        new_address=f"{company_ares.address}, {company_ares.psc}",
+        new_ares_name=company_ares.name,
+        new_business_field=company_ares.business_fields,
+        new_legal_form=company_ares.legal_form,
+        new_size=company_ares.size,
+    )
 
 def process_data(data):
     if different_ico(data):
@@ -19,33 +29,16 @@ def process_data(data):
                 ico = str(company.ico.strip())
                 company_ares = get_company_data_ares(ico)
                 content = czso_get_website_content(ico)
-                if czso_parse_content(content):
-                    ares_main_economic_activity_cz_nace = czso_parse_content(content)[0]
-                    print(company.name)
-                    change_company_data(
-                        company_id=company.id_pipedrive,
-                        new_address=f"{company_ares.address}, {company_ares.psc}",
-                        new_ares_name=company_ares.name,
-                        new_business_field=company_ares.business_fields,
-                        new_legal_form=company_ares.legal_form,
-                        new_size=company_ares.size,
-                    )
+                parsed_content = czso_parse_content(content)
+                print(company.name)
+                process_common_data(company, company_ares)
+                if parsed_content:
+                    ares_main_economic_activity_cz_nace = parsed_content[0]
                     change_main_economic_activity_cz_nace(
                         company_id=company.id_pipedrive,
                         ares_main_economic_activity_cz_nace = ares_main_economic_activity_cz_nace,
                         ares_based_main_economic_activity_cz_nace = czso_get_base_cz_nace(str(ares_main_economic_activity_cz_nace)),
                     )
-                else:
-                    print(company.name)
-                    change_company_data(
-                        company_id=company.id_pipedrive,
-                        new_address=f"{company_ares.address}, {company_ares.psc}",
-                        new_ares_name=company_ares.name,
-                        new_business_field=company_ares.business_fields,
-                        new_legal_form=company_ares.legal_form,
-                        new_size=company_ares.size,
-                    )
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -53,19 +46,15 @@ def index():
     print(f"{current_dateTime} someone hit the site.")
     return 'ok', 200
 
-
-@app.route(f'/{webhook_url}', methods=['POST'])
+@app.route(f'/{WEBHOOK_URL}', methods=['POST'])
 def webhook():
     data = request.get_json()
     if data is None:
-        # If no data is sent or it's not in JSON format, send a "bad request" status.
         return 'Bad Request', 400
 
-    # Spawn a new thread to process the data.
     thread = Thread(target=process_data, args=(data,))
     thread.start()
 
-    # Immediately return a 200 response.
     return '', 200
 
 if __name__ == '__main__':
